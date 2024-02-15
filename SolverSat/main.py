581af910acc26d20ff9bb289266d658e415c69a8
SALHI younes <younessalhi520@gmail.com>
2024-02-15 20:56:29 +0100

    encode ok & main ok

--- SolverSat/main.py
+++ SolverSat/main.py
@@ -1,3 +1,6 @@
+import subprocess
+import sys
+
 #nb de variables = nj * ne * ne
 
 #fonction qui calcule l'indice k d'une variable
@@ -36,9 +39,6 @@ def encoderC1(ne, nj):
                 if (x!=y) : #car une equipe ne joue pas un match avec elle meme
                     l1.append(codage(ne, nj, j, x, y))
                     l1.append(codage(ne, nj, j, y, x))
-            for l in l1:
-                print(str(l)+ ',' +str(decodage(l, ne))+' ', end='')
-            print()
             body += au_plus_un_vrai(l1)
             l1.clear()
     return body
@@ -71,7 +71,7 @@ il suffit de calculer la valeur suivante :
     = n**2- ((n)*(n-1)) // 2
 donc on note nbCAUV(n) = n**2- ((n)*(n-1)) // 2, le nombre de clauses de que retourne 
 la fonction au_plus_un_vrai(l),
-donc la fonction encoderC1(ne, nj) va retourner nj * ne * nbCAUV(ne-2) * 2
+donc la fonction encoderC1(ne, nj) va retourner nj * ne * nbCAUV(2*(ne-2)) * 2
 et pour encoderC2(ne, nj) on aura au final ne * (ne - 1) * 2 contraintes car la premiere 
 boucle parcours ne equipes et la secondes ne-1, et comme la fonction au_moins_un_vrais
 retourne une clause et qu'on l'appel deux fois donc au total on retourne ne*(ne-1)*2 clauses
@@ -83,35 +83,54 @@ def nbCAUV(n):
 
 def encoder(ne, nj):
     nbVar = nj * ne * ne
-    nbClauseC1 = ne * nj * nbCAUV(ne-2) * 2
+    nbClauseC1 = ne * nj * nbCAUV(2*(ne-2)) * 2
     nbClauseC2 = ne * (ne - 1)
     nbTotalClauses = nbClauseC1 + nbClauseC2
     codage = encoderC1(ne, nj) + encoderC2(ne, nj)
     return  "p cnf " + str(nbVar) + " " + str(nbTotalClauses)+ "\n" + codage
 
+#recuperation du nom d'une equipe
+def recuperNomEquipe(f, i):
+    if f is None:
+        return str(i)
+    with open(f, 'r') as file:
+        for index, ligne in enumerate(file):
+            if i == index :
+                return ligne
+
 #pour lire la reponse du resultat dimacs
-def liteReponse(file, ne):
+def liteReponse(file, equipe, ne):
     last_line = None
     with open(file, 'r') as f:
         for ligne in f:
             last_line = ligne.strip()
     resp = last_line.split(' ')
+    if (resp[1] == 'UNSATISFIABLE'):
+        print("pas de solution !!")
+        return
     for r in resp[1:len(resp)-1]:
         r = int(r)
         if (r>=0):
             j, x, y = (decodage(r, ne))
-            print("l'equipe "+str(x)+" joue contre "+str(y)+" le jour "+str(j))
+            print("l'equipe "+recuperNomEquipe(equipe, x)+" joue contre "+recuperNomEquipe(equipe, y)+" le jour "+str(j))
 
-with open('res.pl', 'w') as f:
-    f.write(encoder(3, 4))
-encoderC1(3,4)
-liteReponse('model.txt', 3)
-
-'''
 if __name__ == '__main__':
-    #with open('res.pl', 'w') as f:
-    #    f.write(encoder(3, 4))
-    #liteReponse('model.txt', 3)
-    r = codage(3, 4, 1, 0, 2)
-    print(r)
-    print(decodage(16, 3))'''
+    if (len(sys.argv) < 3):
+        print('Erreur dans le nombre de parametres')
+        print('\tIl faut le nombre d\'equipes et le nombre de jours')
+        print('\t\tpython main.py ne nj')
+        print('\tExemple : ')
+        print('\t\tpython main.py 4 5')
+        sys.exit()
+    
+    ne = int(sys.argv[1])
+    nj = int(sys.argv[2])
+
+    #ecriture dans un fichier du programme pl
+    with open('res.pl', 'w') as f:
+        f.write(encoder(ne, nj))
+    #execution de glucose
+    command = './glucose -modele res.pl > model.txt' 
+    subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    #lecture des reponses
+    liteReponse('model.txt', 'nomEquipe.txt', ne)
